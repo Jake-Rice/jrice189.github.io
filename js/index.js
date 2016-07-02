2f7661b54e021e29f2be68c9b8f989db1902e35d
Jake Rice <jrice189@gmail.com>
2016-07-02 12:59:41 -0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(".project1").hover(function(){on(1)}, function(){off(1)});
+$(".project2").hover(function(){on(2)}, function(){off(2)});
+$(".project3").hover(function(){on(3)}, function(){off(3)});
+$(".project4").hover(function(){on(4)}, function(){off(4)});
+$(".project5").hover(function(){on(5)}, function(){off(5)});
+$(".project6").hover(function(){on(6)}, function(){off(6)});
+$(".project7").hover(function(){on(7)}, function(){off(7)});
+$(".project8").hover(function(){on(8)}, function(){off(8)});
+
+function on(a) {
+  $(".project"+a).css("color","#ff7422");
+  $(".project"+a).css("border-color","#ff7422");
+}
+function off(a) {
+  $(".project"+a).css("color","");
+  $(".project"+a).css("border-color","");
+}
