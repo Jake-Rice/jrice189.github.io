f30f76fe08f2d85c0582aecc90e4012145a51696
Jake Rice <jrice189@gmail.com>
2016-07-12 12:21:26 -0700

    Added new skills to About panel

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-$(".project1").hover(function(){on(1)}, function(){off(1)});
-$(".project2").hover(function(){on(2)}, function(){off(2)});
-$(".project3").hover(function(){on(3)}, function(){off(3)});
-$(".project4").hover(function(){on(4)}, function(){off(4)});
-$(".project5").hover(function(){on(5)}, function(){off(5)});
-$(".project6").hover(function(){on(6)}, function(){off(6)});
-$(".project7").hover(function(){on(7)}, function(){off(7)});
-$(".project8").hover(function(){on(8)}, function(){off(8)});
+$(".project1").hover(function(){on(1);}, function(){off(1);});
+$(".project2").hover(function(){on(2);}, function(){off(2);});
+$(".project3").hover(function(){on(3);}, function(){off(3);});
+$(".project4").hover(function(){on(4);}, function(){off(4);});
+$(".project5").hover(function(){on(5);}, function(){off(5);});
+$(".project6").hover(function(){on(6);}, function(){off(6);});
+$(".project7").hover(function(){on(7);}, function(){off(7);});
+$(".project8").hover(function(){on(8);}, function(){off(8);});
 
 function on(a) {
   $(".ele"+a).css("color","#ff7422");
